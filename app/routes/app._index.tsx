706d3f9308f4c678bae4dd35472a46e8d83e59e8
johnsethsalazar <salazarjohnseth@gmail.com>
2024-06-06 14:50:33 +0800

    Added app initial basic components

---
+++ app/routes/app._index.tsx
@@ -0,0 +1,31 @@
+import { Link } from "@remix-run/react";
+import { Button, Card, Layout, Page, Text } from "@shopify/polaris";
+import React from "react";
+
+type Props = {};
+
+const Index = (props: Props) => {
+  return (
+    <Page>
+      <ui-title-bar title="Bulky operation"></ui-title-bar>
+      <Layout>
+        <Layout.Section>
+          <Card>
+            <Text as="h4" variant="headingMd">
+              Heading
+            </Text>
+            <br/>
+            <Text as="h6">
+              You will be able to select the particular data 
+            </Text>
+            <Link to="/app/exportform">
+              <Button variant="primary">New Export</Button>
+            </Link>
+          </Card>
+        </Layout.Section>
+      </Layout>
+    </Page>
+  );
+};
+
+export default Index;
