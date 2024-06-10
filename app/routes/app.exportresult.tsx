e831b21ec9fcaf11edd5984aba2c98f198cddb4c
johnsethsalazar <salazarjohnseth@gmail.com>
2024-06-10 20:13:55 +0800

    Added some function to dynamically display the data dynamically in the UI but with error: TypeError: Cannot destructure property 'status' of 'fetcher.data' as it is undefined.     at https://speaks-goes-eco-present.trycloudflare.com/app/routes/app.exportresult.tsx?import:26:7     at commitHookEffectListMount (https://speaks-goes-eco-present.trycloudflare.com/node_modules/.vite/deps/chunk-KVSOM4G3.js?v=bee6972c:16936:34)     at invokePassiveEffectMountInDEV (https://speaks-goes-eco-present.trycloudflare.com/node_modules/.vite/deps/chunk-KVSOM4G3.js?v=bee6972c:18352:19)     at invokeEffectsInDev (https://speaks-goes-eco-present.trycloudflare.com/node_modules/.vite/deps/chunk-KVSOM4G3.js?v=bee6972c:19729:19)     at commitDoubleInvokeEffectsInDEV (https://speaks-goes-eco-present.trycloudflare.com/node_modules/.vite/deps/chunk-KVSOM4G3.js?v=bee6972c:19714:15)     at flushPassiveEffectsImpl (https://speaks-goes-eco-present.trycloudflare.com/node_modules/.vite/deps/chunk-KVSOM4G3.js?v=bee6972c:19531:13)     at flushPassiveEffects (https://speaks-goes-eco-present.trycloudflare.com/node_modules/.vite/deps/chunk-KVSOM4G3.js?v=bee6972c:19475:22)     at performSyncWorkOnRoot (https://speaks-goes-eco-present.trycloudflare.com/node_modules/.vite/deps/chunk-KVSOM4G3.js?v=bee6972c:18896:11)     at flushSyncCallbacks (https://speaks-goes-eco-present.trycloudflare.com/node_modules/.vite/deps/chunk-KVSOM4G3.js?v=bee6972c:9135:30)     at commitRootImpl (https://speaks-goes-eco-present.trycloudflare.com/node_modules/.vite/deps/chunk-KVSOM4G3.js?v=bee6972c:19460:11)

--- app/routes/app.exportresult.tsx
+++ app/routes/app.exportresult.tsx
@@ -1,7 +1,7 @@
 import { LoaderFunction, json } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
-import { Page } from "@shopify/polaris";
-import { productsQuery } from "~/graphql/productsQuery";
+import { useFetcher, useLoaderData } from "@remix-run/react";
+import { Banner, Card, Layout, Page, ProgressBar } from "@shopify/polaris";
+import { useEffect, useState } from "react";
 import { authenticate } from "~/shopify.server";
 
 type bulkOperation = {
@@ -48,8 +48,83 @@ export const loader: LoaderFunction = async ({ request }) => {
 const ExportResult = () => {
   const data: bulkOperation = useLoaderData<typeof loader>();
 
+  const [pollingData, setPollingData] = useState(data);
+  const [shouldPoll, setShouldPoll] = useState(true);
+
+  useEffect(() => setPollingData(data), [data]);
+
+  const fetcher = useFetcher();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (document.visibilityState === "visible" && shouldPoll) {
+        fetcher.load("/app/exportresult");
+      }
+    }, 10 * 1000);
+
+    return () => clearInterval(interval);
+  }, [shouldPoll, fetcher.load]);
+
+  useEffect(() => {
+    if (fetcher.data) {
+      setPollingData(fetcher.data as bulkOperation);
+    }
+    
+    const { status } = fetcher.data as bulkOperation;
+
+    if (status === "COMPLETED") {
+      setShouldPoll(false);
+      console.log("------polling stopped------");
+    }
+  }, [fetcher.data]);
+
+  const downloadData = () => {
+    console.log("download");
+  };
+
   console.log(data, "data");
-  return <Page>ExportResult</Page>;
+  return (
+    <Page>
+      <ui-title-bar title="New Export">
+        <button variant="breadcrumb">Bulky</button>
+        <button onClick={() => {}}>Back</button>
+        <button variant="primary">Download Exported File</button>
+      </ui-title-bar>
+      {pollingData.status === "RUNNING" && (
+        <Layout>
+          <Layout.Section>
+            <Banner title="Export in progress">
+              <ProgressBar progress={75} />
+            </Banner>
+            <br />
+            <Card>
+              <p>In progress</p>
+              <p>ID: {pollingData.id}</p>
+              <p>STATUS: {pollingData.status}</p>
+            </Card>
+          </Layout.Section>
+        </Layout>
+      )}
+      {pollingData.status === "COMPLETED" && (
+        <Layout>
+          <Layout.Section>
+            <Banner
+              title="Export Finished"
+              tone="success"
+              action={{ content: "Download", onAction: downloadData }}
+            >
+              <br />
+              <Card>
+                <p>ID: {pollingData.id}</p>
+                <p>STATUS: {pollingData.status}</p>
+                <p>URL: {pollingData.url}</p>
+              </Card>
+            </Banner>
+          </Layout.Section>
+        </Layout>
+      )}
+    </Page>
+  );
 };
 
 export default ExportResult;
